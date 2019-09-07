1b6e045f7b72afd54a17234414f2b57025f7f158
Aminul Islam Nahid <52396238+CodeL4b@users.noreply.github.com>
2019-09-07 17:20:31 +0600

    Add files via upload

---
+++ Variable Data/variabledata.php
@@ -0,0 +1,18 @@
+<?php   
+ 
+$input = 'George'; 
+echo "input is ".gettype($input).".<br/>\n";   
+ 
+$input = 88.9; 
+echo "input is ".gettype($input).".<br/>\n";   
+ 
+$input = true; 
+echo "input is ".gettype($input).".<br/>\n";   
+ 
+$input = 8; 
+echo "input is ".gettype($input).".<br/>\n";
+ 
+$input = null; 
+echo "input is ".gettype($input).".<br/>\n";   
+ 
+?> 
