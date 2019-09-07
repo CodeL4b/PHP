e0fdc04991dee86ab18efb392c9f2c6ecfa970b4
Aminul Islam Nahid <52396238+CodeL4b@users.noreply.github.com>
2019-09-07 17:19:54 +0600

    Add files via upload

---
+++ Variable/variable.php
@@ -0,0 +1,15 @@
+<?php
+	echo "I am a intern. <br/>";
+
+	$I="I";
+	$am="am";
+	$intern="intern";
+
+	echo "$I $am a $intern. <br/>";
+
+	$I="We";
+	$am="are";
+	$intern="team";
+
+	echo "$I $am a $intern. <br/>";
+?>
