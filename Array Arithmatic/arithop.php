ded5fd4ac68f66e380f62758acd44714fc912461
Aminul Islam Nahid <52396238+CodeL4b@users.noreply.github.com>
2019-09-07 17:18:08 +0600

    Add files via upload

---
+++ Array Arithmatic/arithop.php
@@ -0,0 +1,20 @@
+<?php
+
+	$x=20;
+	$y=10;
+
+	$result=$x+$y;
+	echo "$x+$y = $result <br/>";
+
+	$result=$x-$y;
+	echo "$x-$y = $result <br/>";
+
+	$result=$x*$y;
+	echo "$x*$y = $result <br/>";
+
+	$result=$x/$y;
+	echo "$x/$y = $result <br/>";
+
+	$result=$X%$y;
+	echo "$x%$y = $result <br/>";
+?>
