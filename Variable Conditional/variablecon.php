8742c539cafa7762b907ba3bf3da0fcb731383cb
Aminul Islam Nahid <52396238+CodeL4b@users.noreply.github.com>
2019-09-07 17:20:15 +0600

    Add files via upload

---
+++ Variable Conditional/variablecon.php
@@ -0,0 +1,20 @@
+<?php   
+ 
+$name='Nahid'; 
+$age=24;   
+ 
+var_dumps($name); 
+echo "<br/>";   
+ 
+print_r($name); 
+echo "<br/>";   
+ 
+var_dump($age); 
+echo "<br/>";  
+ 
+$name=null;
+
+var_dump($name);
+
+ 
+?> 
