c63add5c31a6a5efea4cd2b9077f891bcded062b
Aminul Islam Nahid <52396238+CodeL4b@users.noreply.github.com>
2019-09-07 17:17:51 +0600

    Add files via upload

---
+++ Array/array.php
@@ -0,0 +1,17 @@
+<?php
+
+$weather=array(
+  "rain",
+  "sunshine",
+  "clouds",
+  "hail",
+  "sleet",
+  "snow",
+  "wind" 
+  );
+ 
+ 
+echo "<p>We've seen all kinds of weather this month. At the beginning of the month, ";
+echo "we had $weather[5] and $weather[6]. Then came $weather[1] with a few $weather[2] ";
+echo "and some $weather[0]. At least we didn't get any $weather[3] or $weather[4].</p>";
+?>
